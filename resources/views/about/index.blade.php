c35137e99aee7abd49d0d25d07b1f841111f6030
tr3nx <0wnedbytr3nx@gmail.com>
2016-01-25 04:33:35 +0000

    fixes to resume

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -3,14 +3,13 @@
 @section('content')
 <div class="page">
     <div class="container content">
-        <h2 class="page-title">Kamil Wierciszewski</h2>
+        <h2 class="page-title">Kamil Wierciszewski<span>Multimedia Designer</span></h2>
 
     	<div class="row">
     		<div class="four columns">
     			<img src="/assets/images/kamil.jpg" style="width:90%">
     		</div>
     		<div class="eight columns">
-    			<h3>Multimedia Designer</h3>
 		        <p>I am a multi-talented designer experienced in interactive web, ui/ux, photo retouching, photography, and print design. During my career, I created mood boards, interactive menus, and icons for web navigation.</p>
 				<p>I had the pleasure of interacting with many inspireing art developers and directors as a team member that has shown and taught me about me the true meaning of planning and design developement.</p>
 				<p>When designing I place a strong emphasis on structure and user experience. The pride I take in creating layouts, that stand out in the eye of the day to day user and  the ability to produce work that can be used by consumers and advertisers, is very rewarding to me. </p>
@@ -34,7 +33,7 @@
 				<p>Programs and categories that I am involved with are 3d graphics programs - Maya, Blender, SFX - after effects, cinema 4d, and Unreal engine. </p>
 
 				<div class="right">
-					<a class="button" href="{{ route('contact.index') }}">Contact Me</a> <a class="button" href="{{ route('about.resume') }}">View My Resum&egrave;</a>
+					<a class="button" href="{{ route('contact.index') }}">Contact Me</a>&nbsp;&nbsp;&nbsp; <a class="button" href="{{ route('about.resume') }}">View My Resum&egrave;</a>
     			</div>
     		</div>
     	</div>
